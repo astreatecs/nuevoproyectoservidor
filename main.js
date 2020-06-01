b8c0c50ebfc31646426bb90b314a6499526e2054
astreatecs <astreatecs@gmail.com>
2020-06-01 17:25:46 -0500

    crear informacion del postman

--- main.js
+++ main.js
@@ -1,28 +1,63 @@
 const express = require('express');
 var bodyParser = require('body-parser');
 let app = express();
-let body = bodyParser();
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+let puerto = process.env.PORT || 3500;
 
-let coneccion = process.env.PORT || 3500;
+//Arreglos
+let Usuarios = [];
+let Recargas = [];
 
 //Rutas GET
 app.get('/GetUsuarios', function(req, res){
-res.status(200).send('Bienvenido');
+    console.log('conectado');
+res.status(200).json(Usuarios);
 res.end();
 });
 
 app.get('/GetRecargas', function(req, res){
-    res.status(200).send('Bienvenido');
-    res.end();
+   let idBuscado = req.body.idUsuario;
+   let validacion = false;
+   Recargas.forEach(Element => {
+       if(Element.idUsuario == idBuscado){
+           validacion = true;
+       }
+   });
+   if (validacion){
+       resp.status(200).send(Recargas);
+   }else{
+       res.status(400).json({mensaje: 'No existe recarga solicitada'})
+   }
+   res.end();
     });
 
 //Rutas POST
-app.post('/', function(req, res){
-    req.body.nombre();
-    req.end();
+app.post('/InsertarUsuario', function(req, res){
+    let newUsuario = {
+        id: req.body.id,
+        nombre: req.body.nombre,
+        pcr: req.body.pcr
+    };
+    Usuarios.push(newUsuario);
+    resp.status(200).json({mensaje: 'Se inserto el Usuario'})
+    res.end();
     });
-    
+
+    app.post('/InsertarRecarga', function(req, res){
+        let newRecargas= {
+            idRecarga: Recargas.length + 1,
+            idUsuario: req.body.idUsuario,
+            monto: req.body.monto,
+            numero: req.body.numero,
+            pcr: req.body.pcr
+        };
+        Recargas.push(newRecargas);
+        res.status(200).json({mensaje: 'Se realizo recarga'});
+        res.end();
+    });
+
 //escuchas
-app.listen(port, function(){
-    console.log('escuchando desde el puerto' + port)
+app.listen(puerto, function(){
+    console.log('escuchando desde el puerto ' + puerto)
 });
